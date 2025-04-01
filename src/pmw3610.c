8935c7993000208ab14b1902cde39d2002187235
papalog <papalog.sales11@gmail.com>
2025-04-02 05:46:23 +0900

    CONFIG_PMW3610_ORIENTATION_0

--- src/pmw3610.c
+++ src/pmw3610.c
@@ -653,17 +653,17 @@ static int pmw3610_report_data(const struct device *dev) {
 #endif
 
     if (IS_ENABLED(CONFIG_PMW3610_ORIENTATION_0)) {
-        x = -raw_x;
-        y = raw_y;
-    } else if (IS_ENABLED(CONFIG_PMW3610_ORIENTATION_90)) {
-        x = raw_y;
-        y = -raw_x;
-    } else if (IS_ENABLED(CONFIG_PMW3610_ORIENTATION_180)) {
         x = raw_x;
         y = -raw_y;
-    } else if (IS_ENABLED(CONFIG_PMW3610_ORIENTATION_270)) {
+    } else if (IS_ENABLED(CONFIG_PMW3610_ORIENTATION_90)) {
         x = -raw_y;
         y = raw_x;
+    } else if (IS_ENABLED(CONFIG_PMW3610_ORIENTATION_180)) {
+        x = -raw_x;
+        y = raw_y;
+    } else if (IS_ENABLED(CONFIG_PMW3610_ORIENTATION_270)) {
+        x = raw_y;
+        y = -raw_x;
     }
 
     if (IS_ENABLED(CONFIG_PMW3610_INVERT_X)) {
